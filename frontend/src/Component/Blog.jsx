5f26f34bf75a5f889e933d20b79f9bef35fe121a
Prajon-Adhikari <prajonadhikari44@gmail.com>
2024-12-10 19:24:01 +0545

    adding like feature on blogs

--- frontend/src/Component/Blog.jsx
+++ frontend/src/Component/Blog.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import image1 from "../assets/blog-hero-image.jpg";
 import blog1 from "../assets/blog1.webp";
 import blog2 from "../assets/blog2.webp";
 import blog3 from "../assets/blog3.webp";
@@ -11,6 +10,76 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Blog() {
+  const initialBlogs = [
+    {
+      id: 1,
+      image: blog1,
+      title: "Awesome food and destination",
+      likeCount: 20,
+      likeClicked: false,
+    },
+    {
+      id: 2,
+      image: blog2,
+      title: "Exploring creativity",
+      likeCount: 15,
+      likeClicked: false,
+    },
+    {
+      id: 3,
+      image: blog3,
+      title: "Technology insights",
+      likeCount: 30,
+      likeClicked: false,
+    },
+    {
+      id: 4,
+      image: blog4,
+      title: "Quality Products",
+      likeCount: 10,
+      likeClicked: false,
+    },
+    {
+      id: 5,
+      image: blog4,
+      title: "Quality Products",
+      likeCount: 10,
+      likeClicked: false,
+    },
+    {
+      id: 6,
+      image: blog3,
+      title: "Quality Products",
+      likeCount: 15,
+      likeClicked: false,
+    },
+    {
+      id: 7,
+      image: blog1,
+      title: "Quality Products",
+      likeCount: 10,
+      likeClicked: false,
+    },
+  ];
+
+  const [blogs, setBlogs] = useState(initialBlogs);
+
+  const handleLikeClick = (id) => {
+    setBlogs((prevBlogs) =>
+      prevBlogs.map((blog) =>
+        blog.id === id
+          ? {
+              ...blog,
+              likeCount: blog.likeClicked
+                ? blog.likeCount - 1
+                : blog.likeCount + 1,
+              likeClicked: !blog.likeClicked,
+            }
+          : blog
+      )
+    );
+  };
+
   return (
     <div className="blog-page">
       <Navbar />
@@ -42,160 +111,36 @@ export default function Blog() {
           </div>
         </div>
         <div className="right-main-blog-page">
-          <div className="blog-container">
-            <img src={blog1} alt="" />
-            <div className="blog-message">
-              <p>FOOD, CUSTOM NOV 17,2020</p>
-              <h4>Awesome food and destination</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut
-                magnam aperiam iste dolorum ad cum excepturi cupiditate illo
-                amet.
-              </p>
-              <div className="blog-user">
-                <div>
-                  <img src={person1} alt="" className="blog-username" />{" "}
-                  <span>Prajon Adhikari</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faHeart} className="heart-icon" />{" "}
-                  <span>20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="blog-container">
-            <img src={blog2} alt="" />
-            <div className="blog-message">
-              <p>FOOD, CUSTOM NOV 17,2020</p>
-              <h4>Awesome food and destination</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut
-                magnam aperiam iste dolorum ad cum excepturi cupiditate illo
-                amet.
-              </p>
-              <div className="blog-user">
-                <div>
-                  <img src={person1} alt="" className="blog-username" />{" "}
-                  <span>Prajon Adhikari</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faHeart} className="heart-icon" />{" "}
-                  <span>20</span>
+          {blogs.map((blog) => (
+            <div className="blog-container" key={blog.id}>
+              <img src={blog.image} alt={blog.title} />
+              <div className="blog-message">
+                <p>FOOD, CUSTOM NOV 17,2020</p>
+                <h4>{blog.title}</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
+                  aut magnam aperiam iste dolorum ad cum excepturi cupiditate
+                  illo amet.
+                </p>
+                <div className="blog-user">
+                  <div>
+                    <img src={person1} alt="" className="blog-username" />{" "}
+                    <span>Prajon Adhikari</span>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faHeart}
+                      className={`heart-icon ${
+                        blog.likeClicked ? "liked" : ""
+                      }`}
+                      onClick={() => handleLikeClick(blog.id)}
+                    />{" "}
+                    <span>{blog.likeCount}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="blog-container">
-            <img src={blog3} alt="" />
-            <div className="blog-message">
-              <p>FOOD, CUSTOM NOV 17,2020</p>
-              <h4>Awesome food and destination</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut
-                magnam aperiam iste dolorum ad cum excepturi cupiditate illo
-                amet.
-              </p>
-              <div className="blog-user">
-                <div>
-                  <img src={person1} alt="" className="blog-username" />{" "}
-                  <span>Prajon Adhikari</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faHeart} className="heart-icon" />{" "}
-                  <span>20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="blog-container">
-            <img src={blog4} alt="" />
-            <div className="blog-message">
-              <p>FOOD, CUSTOM NOV 17,2020</p>
-              <h4>Awesome food and destination</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut
-                magnam aperiam iste dolorum ad cum excepturi cupiditate illo
-                amet.
-              </p>
-              <div className="blog-user">
-                <div>
-                  <img src={person1} alt="" className="blog-username" />{" "}
-                  <span>Prajon Adhikari</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faHeart} className="heart-icon" />{" "}
-                  <span>20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="blog-container">
-            <img src={image1} alt="" />
-            <div className="blog-message">
-              <p>FOOD, CUSTOM NOV 17,2020</p>
-              <h4>Awesome food and destination</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut
-                magnam aperiam iste dolorum ad cum excepturi cupiditate illo
-                amet.
-              </p>
-              <div className="blog-user">
-                <div>
-                  <img src={person1} alt="" className="blog-username" />{" "}
-                  <span>Prajon Adhikari</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faHeart} className="heart-icon" />{" "}
-                  <span>20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="blog-container">
-            <img src={image1} alt="" />
-            <div className="blog-message">
-              <p>FOOD, CUSTOM NOV 17,2020</p>
-              <h4>Awesome food and destination</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut
-                magnam aperiam iste dolorum ad cum excepturi cupiditate illo
-                amet.
-              </p>
-              <div className="blog-user">
-                <div>
-                  <img src={person1} alt="" className="blog-username" />{" "}
-                  <span>Prajon Adhikari</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faHeart} className="heart-icon" />{" "}
-                  <span>20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="blog-container">
-            <img src={image1} alt="" />
-            <div className="blog-message">
-              <p>FOOD, CUSTOM NOV 17,2020</p>
-              <h4>Awesome food and destination</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In aut
-                magnam aperiam iste dolorum ad cum excepturi cupiditate illo
-                amet.
-              </p>
-              <div className="blog-user">
-                <div>
-                  <img src={person1} alt="" className="blog-username" />{" "}
-                  <span>Prajon Adhikari</span>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faHeart} className="heart-icon" />{" "}
-                  <span>20</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <Footer />
